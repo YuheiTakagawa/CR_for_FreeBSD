a7bc14df02cbd796863f1daa457a46016ec028ed
YuheiTakagawa <b1014223@fun.ac.jp>
2017-07-21 17:43:02 +0900

    [modify] changed line

--- getcpuinfo.c
+++ getcpuinfo.c
@@ -15,6 +15,9 @@ int main(int argc, char* argv[]){
 	}
 	pid = atoi(argv[1]);
 
+	struct reg reg;
+	long ins = 0;
+	memset(&reg, 0, sizeof(reg));
 	printf("Attaching to %ld\n", pid);
 	rc = ptrace(PT_ATTACH, pid, NULL, 0);
 	if(rc < 0){
@@ -23,18 +26,12 @@ int main(int argc, char* argv[]){
 	}
 	waitpid(pid, &status, 0);
 
-	struct reg reg;
-	long ins=0;
-
-	memset(&reg, 0, sizeof(reg));
-	
 	rc = ptrace(PT_GETREGS, pid, (caddr_t)&reg, 0);
 	if(rc < 0){
 		perror("ptrace");
 		exit (1);
 	}
 	//rc = ptrace(PT_READ_D, pid, (void *)ins, 0);
-	ptrace(PT_DETACH, pid, NULL, 0);
 	printf("RAX: %lx excuted\n", reg.r_rax);
 	printf("RBX: %lx excuted\n", reg.r_rbx);
 	printf("RCX: %lx excuted\n", reg.r_rcx);
@@ -42,8 +39,16 @@ int main(int argc, char* argv[]){
 	printf("RSI: %lx excuted\n", reg.r_rsi);
 	printf("RDI: %lx excuted\n", reg.r_rdi);
 	printf("RBP: %lx excuted\n", reg.r_rbp);
-	printf("R8: %lx excuted\n", reg.r_r8);
-	printf("R9: %lx excuted\n", reg.r_r9);
+	printf("R8 : %lx excuted\n", reg.r_r8);
+	printf("R9 : %lx excuted\n", reg.r_r9);
+	printf("R10: %lx excuted\n", reg.r_r10);
+	printf("R11: %lx excuted\n", reg.r_r11);
+	printf("R12: %lx excuted\n", reg.r_r12);
+	printf("R13: %lx excuted\n", reg.r_r13);
+	printf("R14: %lx excuted\n", reg.r_r14);
+	printf("R15: %lx excuted\n", reg.r_r15);
+
+	ptrace(PT_DETACH, pid, NULL, 0);
 	printf("Process detached\n");
 	return 0;
 }
