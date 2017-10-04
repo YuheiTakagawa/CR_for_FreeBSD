98ab5f14897cbafeed9c68c389279ce4b4d3eab1
YuheiTakagawa <b1014223@fun.ac.jp>
2017-10-05 00:29:32 +0900

    [add] get data address & stack address from linprocfs/maps. run getall

--- get.sh
+++ get.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
 # get process identify of $1 program running
-pid=`ps ax | grep $1 | grep -v -e grep -e $0  | cut -d' ' -f1`
+pid=`ps ax |\
+    	grep $1 |\
+       	grep -v -e grep -e $0  |\
+       	cut -d' ' -f1`
+
 echo target is $pid
 
-/CR_for_FreeBSD/getregs $pid
+data=`cat /compat/linux/proc/$pid/maps |\
+       	grep -A 1 $1 |\
+       	grep -v $1 |\
+       	cut -d'-' -f1`
+
+echo data address is $data
+
+stack=`cat /compat/linux/proc/$pid/maps |\
+	grep -E  "\\[stack\]"|\
+	cut -d'-' -f1`
+
+echo stack address is $stack
+/CR_for_FreeBSD/getall $pid $data $stack
