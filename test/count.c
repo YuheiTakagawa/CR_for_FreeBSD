0228a0ff8c71c93ddf8db40d31a5589b19564d0b
YuheiTakagawa <b1014223@fun.ac.jp>
2017-10-10 14:58:22 +0900

    [add] test file and source

---
+++ test/count.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(char *args[]){
+	int c;
+	char ch[10] = {'\0'};
+	for(c = 0; c < 5000; c++){
+		snprintf(ch, sizeof(ch),  "Hello %d\n", c);
+		write(1, ch, sizeof(ch));
+		sleep(1);
+	}
+	return 0;
+}
