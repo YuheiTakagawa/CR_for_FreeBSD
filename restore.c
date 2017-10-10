2874a3f4f7e5db694426b90ee99a0184e935c9de
YuheiTakagawa <b1014223@fun.ac.jp>
2017-10-10 13:28:37 +0900

    [add] restore (Manual setregs pid) using only syscall write

--- restore.c
+++ restore.c
@@ -11,8 +11,9 @@
 #define BUFSIZE 1024
 #define PATHBUF 30
 
+#include "getcpu.c"
+#include "getmem.c"
 int target(char *path, char* argv[]);
-int getregs(int pid);
 int setregs(int pid);
 int setmems(int pid);
 
@@ -20,6 +21,7 @@ int setmems(int pid);
 int main(int argc, char* argv[]){
 	int pid;
 	int status;
+	int flag = 0;
 	if(argc < 2){
 		printf("Usage: %s path\n", argv[0]);
 		exit(1);
@@ -43,10 +45,10 @@ int main(int argc, char* argv[]){
 			}
 			if(WIFSTOPPED(status)){
 				printf("stopped:%d\n", WSTOPSIG(status));
-				//setregs(pid);
-				//setmems(pid);
+				setregs(pid);
+				setmems(pid);
+				ptrace(PT_DETACH, pid, (caddr_t)1, 0);
 				printf("finished setting values\n");
-				ptrace(PT_CONTINUE, pid, (caddr_t)1, 0);
 			}else if(WIFEXITED(status)){
 				perror("exited");
 				exit(1);
@@ -73,22 +75,22 @@ int setregs(int pid){
 	struct reg reg;
 
 	reg.r_rax = 0x4;
-	reg.r_rbx = 0x2;
-	reg.r_rcx = 0x4b6402b9c5bfacc2;
-	reg.r_rdx = 0x7fffffffe848;
-	reg.r_rsi = 0x7fffffffea60;
-	reg.r_rdi = 0x7fffffffea70;
-	reg.r_rbp = 0x7fffffffeac0;
-	reg.r_rsp = 0x7fffffffea58;
-	reg.r_rip = 0x80090843a;
+	reg.r_rbx = 0x1;
+	reg.r_rcx = 0x41b5fa;
+	reg.r_rdx = 0xa;
+	reg.r_rsi = 0x7fffffffea80;
+	reg.r_rdi = 0x7fffffffea90;
+	reg.r_rbp = 0x7fffffffead0;
+	reg.r_rsp = 0x7fffffffea78;
+	reg.r_rip = 0x40b79a;
 	reg.r_rflags = 0x203;
 	reg.r_r8 = 0x2;
-	reg.r_r9 = 0x1;
-	reg.r_r10 = 0x400867;
-	reg.r_r11 = 0x202;
-	reg.r_r12 = 0x7fffffffeb48;
-	reg.r_r13 = 0x7fffffffeb60;
-	reg.r_r14 = 0x800bc0cc0;
+	reg.r_r9 = 0x100;
+	reg.r_r10 = 0x0;
+	reg.r_r11 = 0x7fffffffe958;
+	reg.r_r12 = 0x2;
+	reg.r_r13 = 0x7fffffffeb68;
+	reg.r_r14 = 0x7fffffffeb58;
 	reg.r_r15 = 0x1;
 	reg.r_cs = 0x43;
 	reg.r_ss = 0x3b;
@@ -96,9 +98,8 @@ int setregs(int pid){
 	reg.r_es = 0x3b;
 	reg.r_fs = 0x13;
 	reg.r_gs = 0x1b;
-
 	if(ptrace(PT_SETREGS, pid, (caddr_t)&reg, 0) < 0){
-		perror("ptrace");
+		perror("ptrace(PT_SETREGS, ...)");
 		exit(1);
 	}
 	return 0;
@@ -114,10 +115,10 @@ int setmems(int pid){
 	snprintf(filepath, sizeof(filepath), "/proc/%d/mem", pid);
 	write_fd = open(filepath, O_WRONLY);
 
-	snprintf(filepath, sizeof(filepath), "/dump/5017_data.img");
+	snprintf(filepath, sizeof(filepath), "/dump/5739_data.img");
 	read_fd = open(filepath, O_RDONLY);
 
-	lseek(write_fd, 0x601000, SEEK_SET);
+	lseek(write_fd, 0x665000, SEEK_SET);
 
 	while(1){
 		
@@ -130,7 +131,7 @@ int setmems(int pid){
 		}
 	}
 
-	snprintf(filepath, sizeof(filepath), "/dump/5017_stack.img");
+	snprintf(filepath, sizeof(filepath), "/dump/5739_stack.img");
 	read_fd = open(filepath, O_RDONLY);
 
 	lseek(write_fd, 0x7ffffffdf000, SEEK_SET);
