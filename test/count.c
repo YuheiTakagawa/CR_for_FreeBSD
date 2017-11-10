aadab34824fb2c19aac21a1c699b0bca52e5ac43
YuheiTakagawa <b1014223@fun.ac.jp>
2017-11-10 20:04:03 +0900

    [add] restring success? from freebsd dumpfiles

--- test/count.c
+++ test/count.c
@@ -1,11 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
+
 int main(char *args[]){
 	int c;
-	//char ch[50] = {'\0'};
-	char ch[50] = "Hello\n";
-	for(c = 0; c < 5000000; c++){
-		ch[2] ++;
+	char ch[50] = {'\0'};
+	for(c = 0; c < 5000; c++){
+		snprintf(ch, sizeof(ch),  "Hello %d\n", c);
 		write(1, ch, sizeof(ch));
+		sleep(1);
 	}
 	return 0;
 }
