1e1fc30bdc88d292f6d9bdd4ad32cebe1c77ce2e
YuheiTakagawa <b1014223@fun.ac.jp>
2017-11-12 13:19:17 +0900

    [modify] add struct linux registers & changed how to use register struct

--- getcpu.c
+++ getcpu.c
@@ -17,8 +17,39 @@ int getregs(pid_t pid);
  * "pid" is target Process Identify, "fd" is file descriptor of dump file
  */
 
+struct linuxreg{
+  unsigned long int r15;
+  unsigned long int r14;
+  unsigned long int r13;
+  unsigned long int r12;
+  unsigned long int rbp;
+  unsigned long int rbx;
+  unsigned long int r11;
+  unsigned long int r10;
+  unsigned long int r9;
+  unsigned long int r8;
+  unsigned long int rax;
+  unsigned long int rcx;
+  unsigned long int rdx;
+  unsigned long int rsi;
+  unsigned long int rdi;
+  unsigned long int orig_rax;
+  unsigned long int rip;
+  unsigned long int cs;
+  unsigned long int eflags;
+  unsigned long int rsp;
+  unsigned long int ss;
+  unsigned long int fs_base;
+  unsigned long int gs_base;
+  unsigned long int ds;
+  unsigned long int es;
+  unsigned long int fs;
+  unsigned long int gs;
+};
+
 int getregs(pid_t pid){
 	struct reg reg;
+	struct linuxreg linuxreg;
 	int rc;
 	int fd;
 	unsigned long fs_base, gs_base;
@@ -72,9 +103,36 @@ int getregs(pid_t pid){
 	printf("GS : %x excuted\n", reg.r_gs);
 	printf("FSB: %x excuted\n", fs_base);
 	printf("GSB: %x excuted\n", gs_base);
-
-	write(fd, &reg, sizeof(reg));
 	
+	linuxreg.r15 = reg.r_r15;
+	linuxreg.r14 = reg.r_r14;
+	linuxreg.r13 = reg.r_r13;
+	linuxreg.r12 = reg.r_r12;
+	linuxreg.r11 = reg.r_r11;
+	linuxreg.r10 = reg.r_r10;
+	linuxreg.r9  = reg.r_r9;
+	linuxreg.r8  = reg.r_r8;
+	linuxreg.rbp = reg.r_rbp;
+	linuxreg.rbx = reg.r_rbx;
+	linuxreg.rax = reg.r_rax;
+	linuxreg.rcx = reg.r_rcx;
+	linuxreg.rdx = reg.r_rdx;
+	linuxreg.rsi = reg.r_rsi;
+	linuxreg.rdi = reg.r_rdi;
+	linuxreg.orig_rax = reg.r_rax;
+	linuxreg.rip = reg.r_rip;
+	linuxreg.cs  = reg.r_cs;
+      	linuxreg.eflags = reg.r_rflags;
+	linuxreg.rsp = reg.r_rsp;
+	linuxreg.ss  = reg.r_ss;
+	linuxreg.fs_base = fs_base;
+	linuxreg.gs_base = gs_base;
+	linuxreg.ds  = reg.r_ds;
+	linuxreg.es  = reg.r_es;
+	linuxreg.fs  = reg.r_fs;
+	linuxreg.gs  = reg.r_gs;	
+	write(fd, &linuxreg, sizeof(linuxreg));	
+
 	return rc;
 
 }
