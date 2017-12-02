82f6c302571a48b176ae7011ff5d386d9563b67c
YuheiTakagawa <b1014223@fun.ac.jp>
2017-12-03 08:08:48 +0900

    [add] origin regster struct and perrors

--- parasite.c
+++ parasite.c
@@ -11,7 +11,7 @@
 #define BUFSIZE 1024
 #define PATHBUF 30
 
-char code[] = {"\x0f\x05\xCC"};
+long code = 0x0f05cc;
 
 struct reg parasite_setregs(int pid, char* mem){
 	struct reg reg, orig;
@@ -29,8 +29,16 @@ struct reg parasite_setregs(int pid, char* mem){
 	reg.r_rdx = 0x40;
 	reg.r_rip = (unsigned long int)mem;
 	
-	ptrace(PT_SETREGS, pid, (caddr_t)&reg, 1);
-
+	printf("path:%s, rdi:%s\n", path, (char *) reg.r_rdi);
+	printf("rip:%lx\n", reg.r_rip);
+	if(ptrace(PT_SETREGS, pid, (caddr_t)&reg, 1) < 0){
+		perror("ptrace(SETREGS)");
+		exit(1);
+	}
+	if(ptrace(PT_WRITE_I, pid, (caddr_t)mem, code) < 0){
+		perror("ptrace(WRITE_I)");
+		exit(1);
+	}
 	return orig;
 }
 
@@ -49,27 +57,45 @@ int main(int argc, char* argv[]){
 	int flag = 0;
 	
 	char *mem;
+	struct reg orig;
 	pid = atoi(argv[1]);
 
-	ptrace(PT_ATTACH, pid, NULL, 0);
-	printf("Attached\n");
+	if(ptrace(PT_ATTACH, pid, NULL, 0) < 0){
+		perror("ptrace(ATTACH)");
+		exit(1);
+	}
+	printf("Attached PID:%d\n", pid);
 
-	waitpid(pid, &status, 0);
+	if(waitpid(pid, &status, 0) < 0){
+		perror("waitpid");
+		exit(1);
+	}
 
 	if(WIFEXITED(status)){
 	}else if (WIFSTOPPED(status)){
-		printf("stop pid = %d, by signal = %d\n", pid, status);
+		printf("stop PID = %d, by signal = %d\n", pid, WSTOPSIG(status));
 
-	mem = (char*) mmap(NULL, 1024, PROT_WRITE|PROT_READ|PROT_EXEC, MAP_ANONYMOUS |MAP_PRIVATE, -1, 0);
-	if(mem == MAP_FAILED){
-		perror("mmap");
+		mem = (char*) mmap(NULL, 1024, PROT_WRITE|PROT_READ|PROT_EXEC, MAP_ANONYMOUS |MAP_PRIVATE, -1, 0);
+		if(mem == MAP_FAILED){
+			perror("mmap");
+			exit(1);
+		}
+		printf("add mem: %p\n", mem);
+		orig = parasite_setregs(pid, mem);
+		ptrace(PT_CONTINUE, pid, (caddr_t)1, 0);
+	}
+
+	if(waitpid(pid, &status, 0) < 0){
+		perror("waitpid");
 		exit(1);
 	}
-	printf("add mem: %p\n", mem);
-	struct reg orig;
-	orig = parasite_setregs(pid, mem);
-	restore_setregs(pid, orig);
 
+	if(WIFEXITED(status)){
+	}else if(WIFSTOPPED(status)){
+		printf("stop PID = %d, by signal = %d\n", pid, WSTOPSIG(status));
+
+		restore_setregs(pid, orig);
+		ptrace(PT_CONTINUE, pid, (caddr_t)1, 0);
 	}
 
 	return 0;
