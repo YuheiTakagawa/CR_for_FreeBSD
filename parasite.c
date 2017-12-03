9c0ca4ab8a1cc21f6860851e9bad7759d01e7ffd
YuheiTakagawa <b1014223@fun.ac.jp>
2017-12-03 10:59:32 +0900

    [add] completed injection write system call

--- parasite.c
+++ parasite.c
@@ -7,38 +7,89 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFSIZE 1024
 #define PATHBUF 30
 
-long code = 0x0f05cc;
+long code = 0xcc050f;
 
-struct reg parasite_setregs(int pid, char* mem){
-	struct reg reg, orig;
-	char *path = "/tmp.log";
+struct orig{
+	long text;
+	long data;
+	char *addr;
+	struct reg reg;
+};
+
+struct orig *parasite_setregs(int pid, char* mem, struct orig *orig){
+	struct reg reg;
+	char *path = "Inject!\n";
+
 	printf("================================\n");
-	ptrace(PT_GETREGS, pid, (caddr_t)&orig, 1);
-	reg = orig;
-	printf("Evacuation registers: %p\n", orig);
+	ptrace(PT_GETREGS, pid, (caddr_t)&reg, 1);
+	printf("Get registers: \n");
+	orig->reg = reg;
+	printf("Evacuation registers: \n");
 
-	printf("ori:%lx, reg:%lx\n", orig.r_rax, reg.r_rax);
-	printf("%p\n", path);
+	printf("path: %p\n", path);
 
-	reg.r_rax = 5;
-	reg.r_rdi = (unsigned long int)path;
-	reg.r_rdx = 0x40;
-	reg.r_rip = (unsigned long int)mem;
+/* completed write system call injection */
 	
-	printf("path:%s, rdi:%s\n", path, (char *) reg.r_rdi);
+	reg.r_rax = 1;
+	reg.r_rdi = 1;
+	reg.r_rsi = (unsigned long int)path; 
+	reg.r_rdx = sizeof(path);
+	reg.r_r10 = 0x0;	
+	reg.r_r8  = 0x0;
+	reg.r_r9  = 0x0;
+	
+	printf("path:%s, rdi:%d, size:%d\n", path, reg.r_rdi, reg.r_r10);
 	printf("rip:%lx\n", reg.r_rip);
+/* changed memory direct */
+	/* get origal memory */
+	orig->text = ptrace(PT_READ_I, pid, (caddr_t)reg.r_rip, 0);
+	orig->data = ptrace(PT_READ_D, pid, (caddr_t)path, 0);
+	orig->addr = path;
+	/******************************/
+
+	/* injection syscall 0xcc050f */	
+	if(ptrace(PT_WRITE_I, pid, (caddr_t)reg.r_rip, code) < 0){
+		perror("ptrace(WRITE_I)");
+		exit(1);
+	}
+	/******************************/
+
+ 	int* tmp = malloc(sizeof(int));
+/*
+	memset(tmp, 0, 4 + 1);
+ 	memcpy(tmp, code, 4);
+ 	if(ptrace(PT_WRITE_I, pid, (caddr_t)reg.r_rip, *tmp) < 0){
+ 		perror("ptrace(WRITE_I)");
+ 		exit(1);
+ 	}
+*/
+	/* write buffer convert to machine lang*/
+	for(int i = 0; i < strlen(path) / 4 + 1; i++){
+		memset(tmp, 0, 4 + 1);
+		memcpy(tmp, path + i * 4, 4);
+		if(ptrace(PT_WRITE_D, pid, (caddr_t)path + i*4 , *tmp) < 0){
+			perror("ptrace(WRITE_I)");
+			exit(1);
+		}
+	}
+
+	free(tmp);
+
+	printf("orig_text: %lx\n", orig->text);
+	printf("orig_data: %lx\n", orig->data);
+	
+/************************/
+	
 	if(ptrace(PT_SETREGS, pid, (caddr_t)&reg, 1) < 0){
 		perror("ptrace(SETREGS)");
 		exit(1);
 	}
-	if(ptrace(PT_WRITE_I, pid, (caddr_t)mem, code) < 0){
-		perror("ptrace(WRITE_I)");
-		exit(1);
-	}
+
 	return orig;
 }
 
@@ -51,13 +102,18 @@ void restore_setregs(int pid, struct reg orig){
 	printf("restore_registers\n");
 }
 
+void restore_memory(int pid, struct orig *orig){
+	ptrace(PT_WRITE_I, pid, (caddr_t)orig->reg.r_rip, orig->text);
+	ptrace(PT_WRITE_I, pid, (caddr_t)orig->addr, orig->data);
+	
+}
 int main(int argc, char* argv[]){
 	int pid;
 	int status;
 	int flag = 0;
 	
 	char *mem;
-	struct reg orig;
+	struct orig orig;
 	pid = atoi(argv[1]);
 
 	if(ptrace(PT_ATTACH, pid, NULL, 0) < 0){
@@ -81,10 +137,9 @@ int main(int argc, char* argv[]){
 			exit(1);
 		}
 		printf("add mem: %p\n", mem);
-		orig = parasite_setregs(pid, mem);
+		parasite_setregs(pid, mem, &orig);
 		ptrace(PT_CONTINUE, pid, (caddr_t)1, 0);
 	}
-
 	if(waitpid(pid, &status, 0) < 0){
 		perror("waitpid");
 		exit(1);
@@ -94,8 +149,9 @@ int main(int argc, char* argv[]){
 	}else if(WIFSTOPPED(status)){
 		printf("stop PID = %d, by signal = %d\n", pid, WSTOPSIG(status));
 
-		restore_setregs(pid, orig);
-		ptrace(PT_CONTINUE, pid, (caddr_t)1, 0);
+		restore_setregs(pid, orig.reg);
+		restore_memory(pid, &orig);
+		ptrace(PT_DETACH, pid, (caddr_t)1, 0);
 	}
 
 	return 0;
