5b7c329d8bd1a17828efa128ec06bda9d3cd4076
YuheiTakagawa <b1014223@fun.ac.jp>
2017-12-05 09:43:04 +0900

    [add] Added parasite syscall and change stack position of memory layout in restore.c

--- restore.c
+++ restore.c
@@ -7,16 +7,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <elf.h>
+#include <sys/mman.h>
 
 #include "register.c"
 #include "setmem.c"
 #include "ptrace.h"
+#include "parasite_syscall.c"
 #define BUFSIZE 1024
 #define PATHBUF 30
 
 int target(char *path, char* argv[]);
 Elf64_Addr get_entry_point(char* filepath);
 
+void prepare_change_stack(int pid, unsigned long int old_addr, unsigned long int old_size, struct orig *orig){
+	inject_syscall(pid, orig, SYSCALL_ARGS, 11, old_addr, old_size, 0x0, 0x0, 0x0, 0x0);
+}
+
+unsigned long int change_stack(int pid, unsigned long int new_addr, unsigned long int new_size, struct orig *orig){
+	restore_orig(pid, orig);
+	inject_syscall(pid, orig, SYSCALL_ARGS, 9, new_addr, new_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | LINUX_MAP_ANONYMOUS, 0x0, 0x0);
+	return new_addr;
+}
 
 int main(int argc, char* argv[]){
 	int pid, filePid;
@@ -25,6 +36,8 @@ int main(int argc, char* argv[]){
 	char *filepath;
 	long origin_text;
 	Elf64_Addr entry_point;
+	unsigned long int stack_addr;
+	struct orig orig;
 
 	if(argc < 3){
 		printf("Usage: %s <path> <file pid>\n", argv[0]);
@@ -59,11 +72,28 @@ int main(int argc, char* argv[]){
 				}
 				else{
 					printf("stopped:%d\n", WSTOPSIG(status));
-					setmems(pid, filePid);
-					ptrace_write_i(pid, entry_point, origin_text);
-					setregs(pid, filePid);
-					printf("finished setting values\n");
+					if(flag == 1){
+						setregs(pid, filePid);
+						printf("finished setting registers\n");
+						printf("prepare changed stack position in memory layout\n");
+						prepare_change_stack(pid, 0x7ffffffdf000, 0x20000, &orig);
+					}
+					if(flag == 2){
+						printf("changed stack position in memory layout\n");
+						stack_addr = change_stack(pid, 0x7ffffffde000, 0x21000, &orig);
+						printf("stack_addr %lx\n", stack_addr);
+						sleep(1);
+					}
+					if(flag == 3){
+						restore_orig(pid, &orig);
+						setmems(pid, filePid, stack_addr);
+
+						sleep(1);
+			//			while(1);
+					}
+					flag++;
 					ptrace_cont(pid);
+			//		printf("CONTINUE...\n");
 				}
 			}else if(WIFEXITED(status)){
 				perror("exited");
