188acf4f815349fc7cd37a0f0bc6a2d1a8fbe00d
YuheiTakagawa <b1014223@fun.ac.jp>
2017-12-24 22:48:02 +0900

    [add] vm data stack size & address

--- src/getmem.c
+++ src/getmem.c
@@ -19,15 +19,17 @@ int getmem(int read_fd, int dump_fd, long int offset, long int size);
 
 int getmems(pid_t pid, long int dataoffset, long int stackoffset){
 	int read_fd, dump_fd;
-	struct vmsize size;
-	get_vmmap(pid, &size);
+	struct vmds vmds;
+	get_vmmap(pid, &vmds);
 	read_fd = open_read_file(pid);
 	
 	dump_fd = open_dump_file(pid, "data");
-	getmem(read_fd, dump_fd, dataoffset, size.data);
+	//getmem(read_fd, dump_fd, dataoffset, vmds.dsize); //If don't use argument dataoffset, please, remove
+	getmem(read_fd, dump_fd, vmds.daddr, vmds.dsize);
 
 	dump_fd = open_dump_file(pid, "stack");
-	getmem(read_fd, dump_fd, stackoffset, size.stack);
+	//getmem(read_fd, dump_fd, stackoffset, vmds.ssize); //If don't use argument stackoffset, please remove
+	getmem(read_fd, dump_fd, vmds.saddr, vmds.ssize);
 
 	close(read_fd);
 	return 0;
--- src/include/freebsd/getmap.c
+++ src/include/freebsd/getmap.c
@@ -9,39 +9,46 @@
 #include <sys/user.h>
 #include <libprocstat.h>
 
-struct vmsize{
-	unsigned long int data;
-	unsigned long int stack;
+struct vmds{
+	unsigned long int dsize;
+	unsigned long int ssize;
+	unsigned long int daddr;
+	unsigned long int saddr;
 };
 
-void get_vmmap(int pid, struct vmsize* size){
+void get_vmmap(int pid, struct vmds* vmds){
 	struct procstat *prst;
 	struct kinfo_proc *kp;
+	struct kinfo_vmentry *kve, *kv;
 
 	unsigned int count = 0;
 	int page_size;
+
 	page_size = getpagesize();
 
 	prst = procstat_open_sysctl();
 	kp = procstat_getprocs(prst, KERN_PROC_PID, pid, &count);
 	/* Please remove commentout, if you want to get stack offset */
-	/*
-	struct kinfo_vmentry *kve, *kv;
 	kve = procstat_getvmmap(prst, (void *)kp, &count);
 	for(int i = 0; i < count; i++){
 		kv = &kve[i];
+		if(i == 1){
+			vmds->daddr = kv->kve_start;
+			continue;
+		}
+
 		if(kv->kve_flags & KVME_FLAG_GROWS_DOWN){
-			printf("start %d: %lx\n", i, kv->kve_start);
-			printf("end %d: %lx\n", i, kv->kve_end);
+			vmds->saddr = kv->kve_start;
+			continue;
 		}
 	}
-	*/
-	size->data = kp->ki_dsize * page_size;
-	size->stack = kp->ki_ssize * page_size;
+	/************************************************/
+	vmds->dsize = kp->ki_dsize * page_size;
+	vmds->ssize = kp->ki_ssize * page_size;
 
 	printf("stack text: %lx\n", kp->ki_tsize * page_size);
-	printf("stack data: %lx\n", size->data);
-	printf("stack size: %lx\n", size->stack);
+	printf("stack data: %lx\n", vmds->dsize);
+	printf("stack size: %lx\n", vmds->ssize);
 
 	procstat_freevmmap(prst, (void *)kp);
 }
