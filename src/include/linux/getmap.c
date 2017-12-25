2619a714e93afe4ce09a6bb8162aaa241c2b87fd
YuheiTakagawa <b1014223@fun.ac.jp>
2017-12-26 03:43:33 +0900

    [add] Linux get stack size and address

--- src/include/linux/getmap.c
+++ src/include/linux/getmap.c
@@ -2,12 +2,14 @@
 #define __GET_VM_
 
 #include <unistd.h>
-#include <sys/param.h>
-#include <sys/queue.h>
-#include <sys/socket.h>
-#include <sys/sysctl.h>
-#include <sys/user.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 //#include <libprocstat.h>
+
+#define BUF_SIZE 1024
+#define PATH_BUF 1024
 
 struct vmds{
 	unsigned long int dsize;
@@ -17,36 +19,33 @@ struct vmds{
 };
 
 void get_vmmap(int pid, struct vmds* vmds){
-/*	
-	struct procstat *prst;
-	struct kinfo_proc *kp;
-	struct kinfo_vmentry *kve, *kv;
 
-	unsigned int count = 0;
-	int page_size;
+	char buf[BUF_SIZE] = {'\0'};
+	char path[PATH_BUF] = {'\0'};
+	int i = 0;
+	char *str;
 
-	page_size = getpagesize();
+	snprintf(path, PATH_BUF, "/proc/%d/smaps", pid);
+	FILE *fp = fopen(path, "r");
 
-	prst = procstat_open_sysctl();
-	kp = procstat_getprocs(prst, KERN_PROC_PID, pid, &count);
-	kve = procstat_getvmmap(prst, (void *)kp, &count);
-	for(int i = 0; i < count; i++){
-		kv = &kve[i];
+	while(fgets(buf, BUF_SIZE, fp) != NULL){
 		if(i == 1){
-			vmds->daddr = kv->kve_start;
-			continue;
+			str = strtok(buf, " ");
+			str = strtok(NULL, " ");
+			vmds->ssize = atoi(str) * 1024;
+			i++;
+			break;
 		}
 
-		if(kv->kve_flags & KVME_FLAG_GROWS_DOWN){
-			vmds->saddr = kv->kve_start;
-			continue;
+		if(strstr(buf, "[stack]") != NULL){
+			str = strtok(buf, "-");
+			vmds->saddr = strtoul(str, &str, 16);
+			i++;
 		}
-	}
-*/
+	} 	
+
 	vmds->dsize = 0x28000;
-	vmds->ssize = 0x21000;
 	vmds->daddr = 0x6c9000;
-	vmds->saddr = 0x7ffffffde000;
 
 	printf("stack data: %lx\n", vmds->dsize);
 	printf("stack size: %lx\n", vmds->ssize);
