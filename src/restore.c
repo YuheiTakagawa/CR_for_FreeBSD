045007d16c71af7ad2501495ede1ab0db8debd9a
YuheiTakagawa <b1014223@fun.ac.jp>
2018-01-29 01:36:20 +0900

    [add] Find from main function address

--- src/restore.c
+++ src/restore.c
@@ -19,6 +19,18 @@
 #define BUFSIZE 1024
 #define PATHBUF 30
 
+#ifdef __x86_64__
+typedef uint64_t Elf_Addr;
+typedef Elf64_Ehdr Elf_Ehdr;
+typedef Elf64_Shdr Elf_Shdr;
+typedef Elf64_Sym  Elf_Sym;
+#else
+typedef uint32_t Elf_Addr;
+typedef Elf32_Ehdr Elf_Ehdr;
+typedef Elf32_Shdr Elf_Shdr;
+typedef Elf32_Sym  Elf_Sym;
+#endif
+
 int target(char *path, char* argv[]);
 Elf64_Addr get_entry_point(char* filepath);
 
@@ -60,7 +72,6 @@ int main(int argc, char* argv[]){
 	unsigned long int stack_addr;
 	unsigned long int stack_size;
 	int file_offset;
-	//long ret;
 	struct orig orig;
 	struct vmds vmds;
 	char *restore_path = "/dump/hello";
@@ -98,16 +109,14 @@ int main(int argc, char* argv[]){
 	}else{
 		close(fd);
 			waitpro(pid, &status);
-			if(WIFSTOPPED(status)){
-				//	entry_point = get_entry_point(filepath);
-					entry_point = 0x4009ae;
+					entry_point = get_entry_point(filepath);
+			//		entry_point = 0x4009ae;
 					ptrace_read_i(pid, entry_point);
 					ptrace_write_i(pid, entry_point, 0xCC);
 					ptrace_cont(pid);
 					flag++;
-			}
 			waitpro(pid, &status);
-					printf("stopped:%d\n", WSTOPSIG(status));
+					//printf("stopped:%d\n", WSTOPSIG(status));
 						get_vmmap(pid, &vmds);
 						printf("finished setting registers\n");
 						prepare_change_stack(pid, vmds.saddr, vmds.ssize, &orig);
@@ -142,12 +151,68 @@ int target(char *path, char *argv[]){
 	exit(ret);
 }
 
-Elf64_Addr get_entry_point(char* filepath){
-		Elf64_Ehdr header;
-		int fd = open(filepath, O_RDONLY);
-		memset(&header, 0, sizeof(Elf64_Ehdr));
+/* Reference http://d.hatena.ne.jp/rti7743/20170616/1497628434 */
 
-		read(fd, &header, sizeof(header));
-		Elf64_Addr entry = header.e_entry;
-		return entry;
+Elf64_Addr get_entry_point(char* filepath){
+	int fd = open(filepath, O_RDONLY);
+	if (fd < 0){
+		return 0;
+	}
+
+	Elf_Ehdr ehdr;
+	Elf_Shdr shdr;
+	Elf_Shdr shdr_linksection;
+	Elf_Sym  sym;
+	int r = read(fd,&ehdr,sizeof(ehdr));
+	if(r < 0){
+		close(fd);
+		return 0;
+	}
+	if(memcmp(ehdr.e_ident,ELFMAG,SELFMAG) != 0){
+		close(fd);
+		return 0;
+	}
+
+	for(int i = 0 ; i < ehdr.e_shnum ; i++ )
+	{
+		lseek(fd,ehdr.e_shoff + (i * sizeof(shdr)),SEEK_SET);
+		r = read(fd,&shdr,sizeof(shdr));
+		if ( r < sizeof(shdr)){
+			continue;
+		}
+		if ( ! (shdr.sh_type == SHT_SYMTAB || shdr.sh_type == SHT_DYNSYM)){
+			continue;
+		}
+
+		lseek(fd,ehdr.e_shoff + (shdr.sh_link * sizeof(shdr)),SEEK_SET);
+		r = read(fd,&shdr_linksection,sizeof(shdr_linksection));
+		if(r < sizeof(shdr_linksection)){
+			continue;
+		}
+
+		const unsigned int nloop_count = shdr.sh_size / sizeof(sym);
+		for(int n = 0 ; n < nloop_count; n++ ){
+			lseek(fd,shdr.sh_offset + (n*sizeof(sym)),SEEK_SET);
+			r = read(fd,&sym,sizeof(sym));
+			if ( r < sizeof(sym) ){
+				continue;
+			}
+
+			char buf[256];
+			lseek(fd,shdr_linksection.sh_offset + sym.st_name,SEEK_SET);
+			r = read(fd,buf,255);
+			if ( r < 0 ){
+				continue;
+			}
+			buf[r] = 0; 
+			if(!strcmp(buf, "main")){
+				printf("main address: 0x%lx\n", sym.st_value);
+				return sym.st_value;
+			}
+		}
+	}
+
+	close(fd);
+	return 0;
 }
+
