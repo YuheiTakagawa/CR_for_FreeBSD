e24fb98293c17ebe6c473191cf16e87e0968ebda
YuheiTakagawa <b1014223@fun.ac.jp>
2018-01-30 21:30:50 +0900

    [add] test program using heap

---
+++ test/count-heap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(char *args[]){
+	int c;
+	char *ch = malloc(100);
+	for(c = 0; c < 5000; c++){
+		snprintf(ch, sizeof(ch),  "Hello %d\n", c);
+		write(1, ch, sizeof(ch));
+		sleep(1);
+	}
+	return 0;
+}
