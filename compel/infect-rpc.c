73aff7c44fe735701f6c678901b24a7a02b23dec
YuheiTakagawa <b1014223@fun.ac.jp>
2018-02-16 16:41:01 +0900

    [modify] move injection rpc

---
+++ compel/infect-rpc.c
@@ -0,0 +1,95 @@
+//#include "log.h"
+//#include "common/bug.h"
+#include "xmalloc.h"
+//#include "common/lock.h"
+
+//#include "infect.h"
+//#include "infect-priv.h"
+#include "infect-rpc.h"
+#include "rpc-pie-priv.h"
+
+static int __parasite_send_cmd(int sockfd, struct ctl_msg *m)
+{
+	int ret;
+
+
+	ret = send(sockfd, m, sizeof(*m), 0);
+	if (ret == -1) {
+		return -1;
+	} else if (ret != sizeof(*m)) {
+		return -1;
+	}
+
+	return 0;
+}
+
+int parasite_wait_ack(int sockfd, unsigned int cmd, struct ctl_msg *m)
+{
+	int ret;
+
+	while (1) {
+		memzero(m, sizeof(*m));
+
+		ret = recv(sockfd, m, sizeof(*m), MSG_WAITALL);
+		if (ret == -1) {
+			return -1;
+		} else if (ret != sizeof(*m)) {
+			return -1;
+		}
+
+		if (m->cmd != cmd || m->ack != cmd) {
+			return -1;
+		}
+		return 0;
+	}
+
+	return -1;
+}
+
+//int compel_rpc_sync(unsigned int cmd, struct parasite_ctl *ctl)
+int compel_rpc_sync(unsigned int cmd, int tsock)
+{
+	struct ctl_msg m;
+
+	//if (parasite_wait_ack(ctl->tsock, cmd, &m))
+	if (parasite_wait_ack(tsock, cmd, &m))
+		return -1;
+
+	if (m.err != 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
+//int compel_rpc_call(unsigned int cmd, struct parasite_ctl *ctl)
+int compel_rpc_call(unsigned int cmd, int tsock)
+{
+	struct ctl_msg m;
+
+	m = ctl_msg_cmd(cmd);
+	//return __parasite_send_cmd(ctl->tsock, &m);
+	return __parasite_send_cmd(tsock, &m);
+}
+
+//int compel_rpc_call_sync(unsigned int cmd, struct parasite_ctl *ctl)
+int compel_rpc_call_sync(unsigned int cmd, int tsock)
+{
+	int ret;
+
+	//ret = compel_rpc_call(cmd, ctl->tsock);
+	ret = compel_rpc_call(cmd, tsock);
+	if (!ret)
+		//ret = compel_rpc_sync(cmd, ctl->tsock);
+		ret = compel_rpc_sync(cmd, tsock);
+
+	return ret;
+}
+
+/* //Temporarily, commentout until use (struct parasite_ctl)
+int compel_rpc_sock(struct parasite_ctl *ctl)
+{
+	return ctl->tsock;
+}
+*/
+
