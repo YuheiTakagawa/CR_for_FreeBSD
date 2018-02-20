13dad2bdbef05e2c0830f8a9d452b69641af81b0
YuheiTakagawa <b1014223@fun.ac.jp>
2018-02-20 16:46:18 +0900

    [modify] introduce struct parasite_ctl

--- compel/infect.c
+++ compel/infect.c
@@ -62,11 +62,11 @@ void step_debug(int pid){
 	}
 }
 
-void curing(int clsock){
-	compel_rpc_call_sync(PARASITE_CMD_FINI, clsock);
+void curing(struct parasite_ctl *ctl){
+	compel_rpc_call_sync(PARASITE_CMD_FINI, ctl->tsock);
 }
 
-static int parasite_init_daemon(int pid, void *local, void *remote){
+static int parasite_init_daemon(struct parasite_ctl *ctl){
 	struct parasite_init args;
 	struct sockaddr_un saddr;
 	int sockfd, clsock;
@@ -92,17 +92,17 @@ static int parasite_init_daemon(int pid, void *local, void *remote){
 	args.h_addr.sun_family = saddr.sun_family;
 	strncpy(args.h_addr.sun_path, saddr.sun_path, sizeof(saddr.sun_path));
 
-	memcpy(local + parasite_sym__export_parasite_args, (void *)&args, sizeof(args));
+	memcpy(ctl->addr_args, (void *)&args, sizeof(args));
 
-	ptrace_get_regs(pid, &reg);
+	ptrace_get_regs(ctl->rpid, &reg);
 
-	reg.r_rip = (unsigned long int)remote;
-	reg.r_rbp = (unsigned long int)remote + sizeof(parasite_blob);
+	reg.r_rip = (unsigned long int)ctl->remote_map;
+	reg.r_rbp = (unsigned long int)ctl->remote_map + sizeof(parasite_blob);
 	reg.r_rbp += RESTORE_STACK_SIGFRAME;
 	reg.r_rbp += PARASITE_STACK_SIZE;
 
-	ptrace_set_regs(pid, &reg);
-	ptrace_cont(pid);
+	ptrace_set_regs(ctl->rpid, &reg);
+	ptrace_cont(ctl->rpid);
 	/*
 	 *  parasite_run include ptrace SETREGS and CONTINUE, 
 	 *  but this function does not work properly. 
@@ -138,6 +138,7 @@ int main(int argc, char *argv[]){
 
 	struct orig orig;
 	struct reg reg;
+	struct parasite_ctl *ctl;
 
 	
 	void *remote_map, *remote_fd_map, *local_map;
@@ -149,12 +150,13 @@ int main(int argc, char *argv[]){
 	long ret;
 
 	int status;
-	int pid;
 
-       	pid = atoi(argv[1]);
+	ctl = (struct parasite_ctl *) malloc(sizeof(struct parasite_ctl));
 
-	ptrace_attach(pid);
-	waitpro(pid, &status);
+       	ctl->rpid = atoi(argv[1]);
+
+	ptrace_attach(ctl->rpid);
+	waitpro(ctl->rpid, &status);
 
 
 	/* 
@@ -166,29 +168,26 @@ int main(int argc, char *argv[]){
 	 * Third, local process run mmap syscall to share memory.
 	 */
 
-	remote_map = remote_mmap(pid, &orig, (void *) 0x0,
+	remote_map = remote_mmap(ctl->rpid, &orig, (void *) 0x0,
 		       	PAGE_SIZE, PROT_ALL, LINUX_MAP_ANONYMOUS | MAP_SHARED, 0x0, 0x0);
 	printf("remote_map:%lx\n", (off_t)remote_map);
 
 	fd = open(buf, O_RDWR);
 	printf("open file for shared memory: %s, fd: %d\n", buf, fd);
 	
-	inject_syscall_buf(pid, buf, (unsigned long int)remote_map, 0);
-	compel_syscall(pid, &orig, 0x2, &remote_fd,
+	inject_syscall_buf(ctl->rpid, buf, (unsigned long int)remote_map, 0);
+	compel_syscall(ctl->rpid, &orig, 0x2, &remote_fd,
 		       	(unsigned long)remote_map, O_RDWR, 0x0, 0x0, 0x0, 0x0);
 	printf("remote_fd:%ld\n", remote_fd);
 
-	remote_fd_map = remote_mmap(pid, &orig, (void *) 0x0, sizeof(parasite_blob),
+	ctl->remote_map = remote_mmap(ctl->rpid, &orig, (void *) 0x0, sizeof(parasite_blob),
 		       	PROT_ALL, MAP_SHARED | MAP_FILE, remote_fd, 0x0);
-	compel_syscall(pid, &orig, 0x3, &ret, (unsigned long)remote_fd,
+	compel_syscall(ctl->rpid, &orig, 0x3, &ret, (unsigned long)remote_fd,
 		       	0x0, 0x0, 0x0, 0x0, 0x0); 
-	printf("remote_fd_map:%lx\n", (unsigned long int)remote_fd_map);
+	printf("remote_fd_map:%lx\n", (unsigned long int)ctl->remote_map);
 	
-	local_map = mmap(0x0, sizeof(parasite_blob), PROT_ALL, MAP_SHARED, fd, 0);
-	if((int *)&local_map < 0){
-		perror("mmap(2)");
-	}
-	printf("local_map:%p\n", local_map);
+	ctl->local_map = mmap(0x0, sizeof(parasite_blob), PROT_ALL, MAP_SHARED, fd, 0);
+	printf("local_map:%p\n", ctl->local_map);
 
 
 
@@ -197,7 +196,7 @@ int main(int argc, char *argv[]){
 	 *
 	 */
 
-	memcpy(local_map, parasite_blob, sizeof(parasite_blob));
+	memcpy(ctl->local_map, parasite_blob, sizeof(parasite_blob));
 
 
 	/*
@@ -211,14 +210,15 @@ int main(int argc, char *argv[]){
 	 * Prepare communicate to Parasite Engine via socket
 	 *
 	 */
-	int clsock = parasite_init_daemon(pid, local_map, remote_fd_map);
+	ctl->addr_args = ctl->local_map + parasite_sym__export_parasite_args;
+	ctl->tsock = parasite_init_daemon(ctl);
 
 	/*
 	 * send CMD and wait ACK against CMD
 	 */
-	compel_rpc_call_sync(PARASITE_CMD_DUMP_THREAD, clsock);
-	compel_rpc_call_sync(PARASITE_CMD_DUMP_ITIMERS, clsock);
-	compel_rpc_call_sync(PARASITE_CMD_GET_PID, clsock);
+	compel_rpc_call_sync(PARASITE_CMD_DUMP_THREAD, ctl->tsock);
+	compel_rpc_call_sync(PARASITE_CMD_DUMP_ITIMERS, ctl->tsock);
+	compel_rpc_call_sync(PARASITE_CMD_GET_PID, ctl->tsock);
 
 
 	/*
@@ -232,7 +232,7 @@ int main(int argc, char *argv[]){
 	 * return address is shared memory + args address
 	 */
 	struct hello_pid *hellop; 
-	hellop = local_map + parasite_sym__export_parasite_args;
+	hellop = (struct hello_pid*) ctl->addr_args;
 
 	printf("hello: %s\n", hellop->hello);
 	printf("pid: %d\n", hellop->pid);
@@ -243,14 +243,14 @@ int main(int argc, char *argv[]){
 	 * Parasite Daemon run socket closing and curing.
 	 */
 
-	curing(clsock);
+	curing(ctl);
 	/*
 	 * the last command of Parasite Daemon is int3.
 	 */
 	printf("waiting stop\n");
-	waitpro(pid, &status);
+	waitpro(ctl->rpid, &status);
 	printf("stop: %d\n", WSTOPSIG(status));
-	print_regs(pid);
+	print_regs(ctl->rpid);
 
 	
 	/*  want to munmap allocated memory size.
@@ -261,11 +261,11 @@ int main(int argc, char *argv[]){
 	/* maybe, I think restore memory in compel_syscall, this routine is bad. */
 	//compel_syscall(pid, &orig, 0xb, &ret, (unsigned long)remote_fd_map, 0x1000, 0x0, 0x0, 0x0, 0x0);
 	//compel_syscall(pid, &orig, 0xb, &ret, (unsigned long)remote_map, 0x0, 0x0, 0x0, 0x0, 0x0);
-	ptrace_set_regs(pid, &orig.reg);
+	ptrace_set_regs(ctl->rpid, &orig.reg);
 	printf("restore reg\n");
 
 	//ptrace_cont(pid);
 	//while(1){}
-	ptrace_detach(pid);
+	ptrace_detach(ctl->rpid);
 	return 0;
 }
