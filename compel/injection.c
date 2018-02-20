827e2b800f8cf521a9c1e664018be1bdc172644f
YuheiTakagawa <b1014223@fun.ac.jp>
2018-02-20 14:37:02 +0900

    [add] comments

--- compel/injection.c
+++ compel/injection.c
@@ -17,7 +17,9 @@
 #include "infect-priv.h"
 
 #define LINUX_MAP_ANONYMOUS 0x20 //ANONYMOUS of FreeBSD is 0x200, ANONYMOUS of Linux is 0x20
-#define (PROT_ALL PROT_EXEC | PROT_WRITE | PROT_READ) 
+#define PROT_ALL (PROT_EXEC | PROT_WRITE | PROT_READ) 
+#define PARASITE_STACK_SIZE	(16 << 10)
+#define RESTORE_STACK_SIGFRAME 0 // Please Calc
 
 struct hello_pid{
 	char hello[256];
@@ -47,6 +49,19 @@ void inject_syscall_buf(int pid, char *buf, unsigned long int addr, int size){
 	}
 	free(tmp);
 }
+
+void step_debug(int pid){
+	// stepping debug printing registers infinity loop
+	int status;
+	while(1){
+		print_regs(pid);
+		printf("stop: %d\n", WSTOPSIG(status));
+		sleep(1);
+		ptrace_step(pid);
+		waitpro(pid, &status);
+	}
+}
+
 
 int main(int argc, char *argv[]){
 
@@ -78,14 +93,23 @@ int main(int argc, char *argv[]){
 	ptrace_attach(pid);
 	waitpro(pid, &status);
 
+
+	/* 
+	 *
+	 * First, compel mmap syscall to open file for shared memory
+	 * because target process isn't know the file path,
+	 * so write to memory of target process file path. 
+	 * Second, compel target process run mmap syscall map file for shared memory.
+	 * Third, local process run mmap syscall to share memory.
+	 */
+
 	remote_map = remote_mmap(pid, &orig, (void *) 0x0, PAGE_SIZE, PROT_ALL, LINUX_MAP_ANONYMOUS | MAP_SHARED, 0x0, 0x0);
 	printf("remote_map:%lx\n", (off_t)remote_map);
 
 	fd = open(buf, O_RDWR);
-	printf("open: %s, fd: %d\n", buf, fd);
+	printf("open file for shared memory: %s, fd: %d\n", buf, fd);
 	
 	inject_syscall_buf(pid, buf, (unsigned long int)remote_map, 0);
-	printf("p:%p, lx%lx\n", buf, (unsigned long int)buf);
 	compel_syscall(pid, &orig, 0x2, &remote_fd, (unsigned long)remote_map, O_RDWR, 0x0, 0x0, 0x0, 0x0);
 	printf("remote_fd:%ld\n", remote_fd);
 
@@ -98,84 +122,131 @@ int main(int argc, char *argv[]){
 		perror("mmap(2)");
 	}
 	printf("local_map:%p\n", local_map);
-	//inject_syscall_buf(pid, (char *)parasite_blob, (unsigned long int)remote_fd_map, sizeof(parasite_blob));
+
+
+
+	/*
+	 * Injection Parasite Engine via shared memory.
+	 *
+	 */
+
 	memcpy(local_map, parasite_blob, sizeof(parasite_blob));
 
-/*// steping debug
- * while(1){
-		print_regs(pid);
-		printf("stop: %d\n", WSTOPSIG(status));
-		sleep(1);
-		ptrace_step(pid);
-		waitpro(pid, &status);
-	}
-*/
+
+	/*
+	 * If you want to debug register, please uncomment
+	 */
+	//step_debug(pid);
+
+
+
+	/*
+	 * Prepare communicate to Parasite Engine via socket
+	 *
+	 */
+
+	struct parasite_init args;
 	struct sockaddr_un saddr;
-	//int sockfd = socket(AF_UNIX, SOCK_SEQPACKET | SOCK_NONBLOCK, 0);
-	int sockfd = socket(PF_LOCAL, SOCK_SEQPACKET, 0);
+	int sockfd, clsock;
+	int socklen;
+	struct ctl_msg m = { };
+	struct hello_pid *hellop; 
+
+	//sockfd = socket(AF_UNIX, SOCK_SEQPACKET | SOCK_NONBLOCK, 0); //SOCK_NONBLOCK 
+	sockfd = socket(PF_LOCAL, SOCK_SEQPACKET, 0);
+
 	saddr.sun_family = PF_LOCAL;
 	snprintf(saddr.sun_path, UNIX_PATH_MAX, "crtools-pr-%d", getpid());
-	//strncpy(saddr.sun_path, "testpath", 108);
-	//strcpy(saddr.sun_path, "testpath");
-	int socklen = sizeof(saddr);
+	
+	socklen = sizeof(saddr);
 
 	if(bind(sockfd, (struct sockaddr *)&saddr, socklen) < 0)
 		perror("bind");
 	
 	if(listen(sockfd, 5) < 0)
 		perror("listen");
-	struct parasite_init args;
-	//args.h_addr_len = sizeof(args.h_addr);
+
 	args.h_addr_len = socklen;
 	args.h_addr.sun_family = saddr.sun_family;
 	strncpy(args.h_addr.sun_path, saddr.sun_path, sizeof(saddr.sun_path));
-	//memcpy((void *)&args.h_addr, (void *)&saddr, sizeof(saddr));
 
 	memcpy(local_map + parasite_sym__export_parasite_args, (void *)&args, sizeof(args));
-	printf("%s\n", args.h_addr.sun_path);
 
-	int clsock;
-	//ptrace_cont(pid);
 	ptrace_get_regs(pid, &reg);
 	memcpy(&orireg, &reg, sizeof(reg));
+
 	reg.r_rip = (unsigned long int)remote_fd_map;
-	#define PARASITE_STACK_SIZE	(16 << 10)
-	#define RESTORE_STACK_SIGFRAME 0 // Please Calc
 	reg.r_rbp = (unsigned long int)remote_fd_map + sizeof(parasite_blob);
 	reg.r_rbp += RESTORE_STACK_SIGFRAME;
 	reg.r_rbp += PARASITE_STACK_SIZE;
+
+
 	ptrace_set_regs(pid, &reg);
 	ptrace_cont(pid);
+	/*
+	 *  parasite_run include ptrace SETREGS and CONTINUE, 
+	 *  but this function does not work properly. 
+	 */
 	//parasite_run(pid, PT_CONTINUE, reg.r_rip, (void *)reg.r_rbp, &reg, &orig);
-	printf("waiting\n");
+	
+	printf("connection waiting\n");
+
 	clsock = accept(sockfd, NULL, 0); 
-	//int clsock = accept(sockfd, NULL, 0); 
 	if(clsock < 0){
 		perror("accept");
 		sleep(1);
 	}
-	/*char ch[1024];
-	read(clsock, ch, sizeof(ch));
-	printf("client msg: %s\n", ch);
-	strncpy(ch, "HELLO, I'm LOCAL\n", sizeof(ch));
-	write(clsock, ch, sizeof(ch));
-	*/
-	struct ctl_msg m = { };
+
+	/*
+	 * waiting receive PARASITE_CMD_INIT_DAEMON from Parasite Engine
+	 */
 	parasite_wait_ack(clsock, PARASITE_CMD_INIT_DAEMON, &m);
+
+	/*
+	 * send CMD and wait ACK against CMD
+	 */
 	compel_rpc_call_sync(PARASITE_CMD_DUMP_THREAD, clsock);
 	compel_rpc_call_sync(PARASITE_CMD_DUMP_ITIMERS, clsock);
 	compel_rpc_call_sync(PARASITE_CMD_GET_PID, clsock);
+
+
+	/*
+	 * Wait for Parasite Engine finishes writing to memory.
+	 * Good method is sync, lock, futex.
+	 * Now implement is sleeping process
+	 */
 	usleep(20);
-	struct hello_pid *hellop = local_map + parasite_sym__export_parasite_args;
+
+	/* 
+	 * return address is shared memory + args address
+	 */
+	hellop = local_map + parasite_sym__export_parasite_args;
+
 	printf("hello: %s\n", hellop->hello);
 	printf("pid: %d\n", hellop->pid);
+
+
+	/*
+	 * send PARASITE_CMD_FINI to Parasite Daemon,
+	 * Parasite Daemon run socket closing and curing.
+	 */
 	compel_rpc_call_sync(PARASITE_CMD_FINI, clsock);
+
+
+	/*
+	 * the last command of Parasite Daemon is int3.
+	 */
 	printf("waiting stop\n");
 	waitpro(pid, &status);
 	printf("stop: %d\n", WSTOPSIG(status));
 	print_regs(pid);
+
 	
-	/*  want to munmap allocated memory size. compel_syscall() use remote_map, so can't unmap address remote_map. Please, munmap in Parasite engine itself   */
+	/*  want to munmap allocated memory size.
+	 *  compel_syscall() use remote_map, so can't
+	 *  unmap address remote_map.
+	 *  Please, munmap in Parasite engine itself 
+	 */
 	/* maybe, I think restore memory in compel_syscall, this routine is bad. */
 	//compel_syscall(pid, &orig, 0xb, &ret, (unsigned long)remote_fd_map, 0x1000, 0x0, 0x0, 0x0, 0x0);
 	//compel_syscall(pid, &orig, 0xb, &ret, (unsigned long)remote_map, 0x0, 0x0, 0x0, 0x0, 0x0);
