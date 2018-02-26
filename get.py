a8007c96cefd452f469bb94684cea865ec0ece20
YuheiTakagawa <b1014223@fun.ac.jp>
2018-02-26 20:33:16 +0900

    [modify] get.py

--- get.py
+++ get.py
@@ -19,38 +19,6 @@ print("PID: " + pid)
 ret = subprocess.check_output(["/CR_for_FreeBSD/getall", pid])
 
 print(ret.decode('utf-8'))
-"""
-if platform.system() == 'Linux':
-# get fd information from procfs(5)
-	prst = subprocess.check_output(["ls", "-l", "/proc/" + pid + "/fd"]).decode('utf-8')
-	prst = prst.split("\n")
-	tmp = prst[-2].split(" ")
-	fd = tmp[8]
-	name = tmp[10]
-	tmp = subprocess.check_output(["cat", "/proc/" + pid + "/fdinfo/" + fd]).decode('utf-8')
-	tmp = tmp.split("\n")
-	print(tmp)
-
-	offset = tmp[0].split(" ")[-1]
-	print("======================")
-	print("FD: " + fd)
-	print("OFFSET: " + offset)
-	print("NAME: " + name)
-else:
-# get fd information from procstat(1) 
-	prst = subprocess.check_output(["procstat", "-f", pid]).decode('utf-8')
-	prst = prst.split("\n")
-	tmp = prst[0].split()
-
-	row = [tmp.index("FD"), tmp.index("OFFSET"), tmp.index("NAME")]
-
-	tmp = prst[-2].split()
-	print("======================")
-	print("FD: " + tmp[row[0]])
-	print("OFFSET: " + tmp[row[1]])
-	print("NAME: " + tmp[row[2]])
-"""
-
 subprocess.check_output(["kill", "-TERM", pid])
 print("======================")
 
