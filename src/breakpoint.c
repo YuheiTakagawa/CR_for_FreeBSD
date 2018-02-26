758866eb7d0cdd49fc653ff415ab9740171724bb
YuheiTakagawa <b1014223@fun.ac.jp>
2018-02-26 20:11:37 +0900

    [modify] add comment

--- src/breakpoint.c
+++ src/breakpoint.c
@@ -3,7 +3,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <elf.h>
-
+ 
 #include "ptrace.h"
 #include "common.h"
 #include "breakpoint.h"
@@ -23,20 +23,37 @@ Elf64_Addr get_entry_point(char* filepath){
 	Elf_Shdr shdr;
 	Elf_Shdr shdr_linksection;
 	Elf_Sym  sym;
-	int r = read(fd,&ehdr,sizeof(ehdr));
+	int r = read(fd, &ehdr, sizeof(ehdr));
 	if(r < 0){
 		close(fd);
 		return 0;
 	}
-	if(memcmp(ehdr.e_ident,ELFMAG,SELFMAG) != 0){
+	if(memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0){
 		close(fd);
 		return 0;
 	}
 
-	for(int i = 0 ; i < ehdr.e_shnum ; i++ )
+	/*
+	 * e_shnum is number of section header table entry.
+	 * loop as much as e_shnum.
+	 * 1. read each section header.
+	 * 	 address of section header is 
+	 * 	 elf header + e_shnum * size section header
+	 *
+	 * 2. compare shdr_type to SYMTAB to search section header.
+	 * 	 SYMTAB shdr has symbol table which express function name.
+	 *
+	 * 3. get entity of target section header from link.
+	 *
+	 * 4. Reading and searching symbol table entry which
+	 *    has "main" function.
+	 *
+	 * 5. get address(st_value) searched in step4.
+	 */ 
+	for(int i = 0 ; i < ehdr.e_shnum ; i++)
 	{
-		lseek(fd,ehdr.e_shoff + (i * sizeof(shdr)),SEEK_SET);
-		r = read(fd,&shdr,sizeof(shdr));
+		lseek(fd,ehdr.e_shoff + (i * sizeof(shdr)), SEEK_SET);
+		r = read(fd, &shdr, sizeof(shdr));
 		if ( r < sizeof(shdr)){
 			continue;
 		}
@@ -44,23 +61,26 @@ Elf64_Addr get_entry_point(char* filepath){
 			continue;
 		}
 
-		lseek(fd,ehdr.e_shoff + (shdr.sh_link * sizeof(shdr)),SEEK_SET);
-		r = read(fd,&shdr_linksection,sizeof(shdr_linksection));
+		lseek(fd, ehdr.e_shoff + (shdr.sh_link * sizeof(shdr)), SEEK_SET);
+		r = read(fd, &shdr_linksection, sizeof(shdr_linksection));
 		if(r < sizeof(shdr_linksection)){
 			continue;
 		}
 
+		/*
+		 * Searching "main" symbol and getting main function address
+		 */
 		const unsigned int nloop_count = shdr.sh_size / sizeof(sym);
-		for(int n = 0 ; n < nloop_count; n++ ){
-			lseek(fd,shdr.sh_offset + (n*sizeof(sym)),SEEK_SET);
-			r = read(fd,&sym,sizeof(sym));
-			if ( r < sizeof(sym) ){
+		for(int n = 0 ; n < nloop_count; n++){
+			lseek(fd, shdr.sh_offset + (n * sizeof(sym)), SEEK_SET);
+			r = read(fd, &sym, sizeof(sym));
+			if ( r < sizeof(sym)){
 				continue;
 			}
 
 			char buf[256];
-			lseek(fd,shdr_linksection.sh_offset + sym.st_name,SEEK_SET);
-			r = read(fd,buf,255);
+			lseek(fd, shdr_linksection.sh_offset + sym.st_name, SEEK_SET);
+			r = read(fd, buf, 255);
 			if ( r < 0 ){
 				continue;
 			}
@@ -86,8 +106,6 @@ void insert_breakpoint(pid_t pid, char *elfpath){
 	waitpro(pid, &status);
 	entry_point = get_entry_point(elfpath);
 	ptrace_swap_area(pid, (void *)entry_point, (void *)code_orig, sizeof(code_orig));
-	//ptrace_poke_area(pid, (void *)code_orig,
-	//			(void *)entry_point, sizeof(code_orig));
 	ptrace_cont(pid);
 }
 	
