670d55e79e55821fe4d1d4746037b37c121950f2
YuheiTakagawa <b1014223@fun.ac.jp>
2018-04-02 20:03:12 +0900

    [add] dup redirect

--- testpy/include/syscall.h
+++ testpy/include/syscall.h
@@ -4,12 +4,14 @@
 extern long sys_fork(void);
 extern long sys_write(int fd, const void *buf, unsigned long count);
 extern long sys_read(int fd, const void *buf, unsigned long count);
+extern long sys_open(const char* path, int flags);
 extern long sys_close(int fd);
 extern long sys_getpid(void);
 extern long sys_socket(int domain, int type, int protocol);
 extern long sys_connect(int sockfd, struct sockaddr *addr, int addrlen);
 extern long sys_sendto(int sockfd, void *buff, size_t len, unsigned int flags, struct sockaddr *addr, int addr_len);
 extern long sys_recvfrom(int sockfd, void *buf, size_t len, unsigned int flags, struct sockaddr *addr, int addr_len);
+extern long sys_dup2(int oldfd, int newfd);
 extern long sys_nanosleep(const struct timespec *tp, struct timespec *rem);
 
 #endif
--- testpy/pie/syslib.c
+++ testpy/pie/syslib.c
@@ -3,6 +3,7 @@
 #include <sys/un.h>
 #include <stdarg.h>
 #include <time.h>
+#include <fcntl.h>
 
 #include "syscall.h"
 #include "string.c"
@@ -18,12 +19,29 @@ int connection(void *data){
 	struct timespec tp;
 	tp.tv_sec = 5;
 	tp.tv_nsec = 0;
-	pid = sys_fork();
+	char outpath[] = "/hey";
+	//char inpath[] = "/b.tmp";
+	int outfd = sys_open(outpath, O_RDWR);
+
+	sys_dup2(outfd, 1);
+
+	sys_close(outfd);
+	
+//	int infd = sys_open(inpath, O_RDWR);
+
+//	sys_dup2(infd, 0);
+/*
+ * This is multi process for writing
+ */
+/*	pid = sys_fork();
 	if(pid == 0){
 		while(1){
 			sys_write(0, st, 15); 
 			sys_nanosleep(&tp, NULL);
 		}
 	}
+*/
+
+
 	return 0;
 }
