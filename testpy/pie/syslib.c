b4a02a4258813e100648d2ac9603961c8b05f50e
YuheiTakagawa <b1014223@fun.ac.jp>
2018-04-02 21:19:11 +0900

    [add] get input from keyboard

--- testpy/pie/syslib.c
+++ testpy/pie/syslib.c
@@ -13,9 +13,9 @@
 #define NULL ((void *)0)
 
 int connection(void *data){
-	char st[] = "I'M TAKAGAWA!\n";
-	int pid;
-	int size;
+	char st[] = "print(\"HELLO\")\n";
+	int pid, pid2;
+	int size = 0;
 /*
 	int sock;
 	struct sockaddr_in *sockaddr = data;
@@ -29,15 +29,14 @@ int connection(void *data){
 	sys_dup2(sock, 1);
 	sys_write(1, "Hacked\n", sizeof("Hacked\n"));
 */
-	int fds[2];
+	int fds[2], fds2[2];
 	sys_pipe2(fds, O_CLOEXEC);
-	std_printf("fds[0]:%d, fds[1]:%d\n", fds[0], fds[1]);
 	pid = sys_fork();
 	if(pid == 0){
-		std_printf("child %d\n", sys_getpid());
 		sys_close(fds[1]);
 		sys_close(0);
 		sys_dup2(fds[0], 0);
+		sys_close(fds[0]);
 		char buf[32];
 		struct sockaddr_in *sockaddr = data;
 		int sock;
@@ -53,10 +52,52 @@ int connection(void *data){
 			sys_write(1, buf, sizeof(buf));
 		}
 	}else{
-		std_printf("parent %d\n", sys_getpid());
 		sys_close(fds[0]);
 		sys_close(1);
 		sys_dup2(fds[1], 1);
+		sys_close(fds[1]);
+
+		sys_pipe2(fds2, O_CLOEXEC);
+		pid2 = sys_fork();
+		if(pid2 == 0){
+			char buf[32];
+			sys_close(fds2[0]);
+			struct sockaddr_in *sockaddr = data;
+			int sock;
+			sock = sys_socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+			sys_connect(sock, (struct sockaddr *) sockaddr, sizeof(*sockaddr));	
+			while(1){
+				memset(buf, 0, sizeof(buf));
+
+				for(int i = 0, size = 0; i < sizeof(buf); i++){
+					size += sys_read(0, &buf[i], sizeof(char));
+					sys_write(1, &buf[i], 1);
+					if(buf[i] == '\r'){
+						buf[i] = '\n';
+						sys_write(1, "\n", 1);
+					//	sys_write(sock, buf, i + 1);
+						sys_write(fds2[1], buf, i + 1);
+						break;
+					}
+/*					std_printf("%c\n", buf[i]);
+
+					if(buf[i] == '\0' || buf[i] == '\n'){
+						buf[i] = '\n';
+						break;
+					}
+					*/
+				}
+
+				if(size < 0)
+					break;
+			}
+		}else{
+			sys_close(0);
+			sys_close(fds2[1]);
+			sys_dup2(fds2[0], 0);
+			sys_close(fds2[0]);
+		}
+
 	}
 /*
 	char outpath[] = "/hey";
