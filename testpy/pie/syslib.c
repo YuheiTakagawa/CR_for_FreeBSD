fe039e75da361f8417c2c43c55b8d65722987ec2
YuheiTakagawa <b1014223@fun.ac.jp>
2018-04-04 20:41:44 +0900

    [add] Use pipe. output result to python process and socket

--- testpy/pie/syslib.c
+++ testpy/pie/syslib.c
@@ -15,7 +15,8 @@
 int connection(void *data){
 	char st[] = "I'M TAKAGAWA!\n";
 	int pid;
-
+	int size;
+/*
 	int sock;
 	struct sockaddr_in *sockaddr = data;
 
@@ -24,12 +25,39 @@ int connection(void *data){
 
 	sys_connect(sock, (struct sockaddr *) sockaddr, sizeof(*sockaddr));
 
+	sys_close(1);
 	sys_dup2(sock, 1);
-//	sys_sendto(sock, "aaaa", sizeof("aaaa"));
-
-
-
-
+	sys_write(1, "Hacked\n", sizeof("Hacked\n"));
+*/
+	int fds[2];
+	sys_pipe2(fds, O_CLOEXEC);
+	std_printf("fds[0]:%d, fds[1]:%d\n", fds[0], fds[1]);
+	pid = sys_fork();
+	if(pid == 0){
+		std_printf("child %d\n", sys_getpid());
+		sys_close(fds[1]);
+		sys_close(0);
+		sys_dup2(fds[0], 0);
+		char buf[32];
+		struct sockaddr_in *sockaddr = data;
+		int sock;
+		sock = sys_socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+		sys_connect(sock, (struct sockaddr *) sockaddr, sizeof(*sockaddr));
+		
+		while(1){
+			memset(buf, 0, sizeof(buf));
+			size = sys_read(0, buf, sizeof(buf));
+			if(size < 0)
+				break;
+			sys_write(sock, buf, sizeof(buf));
+			sys_write(1, buf, sizeof(buf));
+		}
+	}else{
+		std_printf("parent %d\n", sys_getpid());
+		sys_close(fds[0]);
+		sys_close(1);
+		sys_dup2(fds[1], 1);
+	}
 /*
 	char outpath[] = "/hey";
 	char inpath[] = "/b.tmp";
