8d5e56a319c0a4b6d816573fe72e13546992de5c
YuheiTakagawa <roto11fon@gmail.com>
2018-05-10 07:49:34 +0900

    [modify] soccr test client

--- soccr/soccrtest/client.c
+++ soccr/soccrtest/client.c
@@ -13,25 +13,27 @@ int main (void){
 	int sockfd;
 	struct sockaddr_in addr;
 	char buf[] = "HELLO WORLD\n";
-	char tmp[13];
+	char tmp[9];
 	int count = 0;
 	srand(time(NULL));
         sockfd	= socket(AF_INET, SOCK_STREAM, 0);
 	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = inet_addr("192.168.11.1");
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addr.sin_port = htons(9090);
 
-	connect(sockfd, (struct sockaddr *) &addr, sizeof(addr));
+	if(connect(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+		perror("connect");
 
-	write(sockfd, buf, sizeof(buf));
 
-	while(count < 500){
+	if(write(sockfd, buf, sizeof(buf)) < 0)
+		perror("write");
+
+	while(read(sockfd, tmp, sizeof(tmp))){
 		//snprintf(buf, sizeof(buf), "HELLO %05d\n", count++);
 		//write(sockfd, buf, sizeof(buf));
-		memset(tmp, '\0', sizeof(tmp));
-		recv(sockfd, tmp, sizeof(tmp), MSG_WAITALL);
 		printf("from sv: %s\n", tmp);
-		usleep(100000);
+//		usleep(100000);
+	//	memset(tmp, '\0', sizeof(tmp));
 	}
 
 	return 0;
