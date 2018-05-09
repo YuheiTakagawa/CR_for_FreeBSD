de2fd08f4cc85247299cb413d081d39b562e9131
YuheiTakagawa <roto11fon@gmail.com>
2018-05-10 08:03:25 +0900

    [add] drain fd

--- compel/pie/syscall.h
+++ compel/pie/syscall.h
@@ -1,12 +1,15 @@
 #ifndef ASM_SYSCALL_PROTO_H_64__
 #define ASM_SYSCALL_PROTO_H_64__
 
+struct linux_msghdr;
+
 extern long sys_write(int fd, const void *buf, unsigned long count);
 extern long sys_read(int fd, const void *buf, unsigned long count);
 extern long sys_close(int fd);
 extern long sys_getpid(void);
-extern long sys_sendmsg(int sockfd, const struct msghdr *msg, int flags);
 extern long sys_recvmsg(int sockfd, struct msghdr *msg, int flags);
+extern long sys_sendmsg(int sockfd, const struct linux_msghdr *msg, int flags);
+//extern long sys_sendmsg(int sockfd, const struct msghdr *msg, int flags);
 extern long sys_socket(int domain, int type, int protocol);
 extern long sys_connect(int sockfd, struct sockaddr *addr, int addrlen);
 extern long sys_sendto(int sockfd, void *buff, size_t len, unsigned int flags, struct sockaddr *addr, int addr_len);
--- compel/pie/syslib.c
+++ compel/pie/syslib.c
@@ -3,7 +3,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <stdarg.h>
+#include <unistd.h>
 
 #include "syscall.h"
 #include "string.c"
@@ -25,14 +25,21 @@ struct parasite_init{
 	struct sockaddr_un h_addr;
 };
 
-static int fill_fds_opts(struct parasite_drain_fd *fds, struct fd_opts *opts)
-{
-	opts->fown.uid = 0;
-	opts->fown.euid = 0;
-	opts->fown.pid_type = 0;
-	opts->fown.pid = 0;
-	return 0;
-}
+struct linux_msghdr {
+	void 		*msg_name;
+	socklen_t	msg_namelen;
+	struct iovec	*msg_iov;
+	size_t		msg_iovlen;
+	void		*msg_control;
+	size_t		msg_controllen;
+	int		msg_flags;
+};
+
+struct linux_cmsghdr {
+	size_t		cmsg_len;
+	int		cmsg_level;
+	int		cmsg_type;
+};
 
 static int __parasite_daemon_reply_ack(unsigned int cmd, int err)
 {
@@ -72,6 +79,109 @@ static int fini(void){
 	return -1;
 }
 
+struct linux_sockaddr_un {
+	unsigned short sun_family;
+	char sun_path[108];
+};
+
+int connect_gate(const char* path)
+{
+	int gate;
+	int ret;
+	struct linux_sockaddr_un gate_addr;
+	/* SOCK_DGRAMを使う */
+	if ( (gate = sys_socket(PF_UNIX, SOCK_DGRAM, 0)) < 0 ) {
+		return -1;
+	}
+	memset(&gate_addr, 0, sizeof(gate_addr));
+	gate_addr.sun_family = PF_UNIX;
+	memcpy(gate_addr.sun_path, path, sizeof(gate_addr.sun_path));
+	*(gate_addr.sun_path + sizeof(gate_addr.sun_path)) = '\0';
+	if ((ret = sys_connect(gate, (struct sockaddr*)&gate_addr, sizeof(gate_addr))) < 0 ) {
+		std_printf("fault connect %d\n", ret);
+		return -1;
+	}
+	std_printf("fini connect %d\n", gate);
+	return gate;
+}
+
+
+/*
+int recvfd(int gate, void* message, size_t message_len)
+{
+	struct msghdr msg;
+	struct iovec iov;
+	char cmsgbuf[CMSG_SPACE(sizeof(int))];
+
+	iov.iov_base = message;
+	iov.iov_len = message_len;
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = cmsgbuf;
+	msg.msg_controllen = sizeof(cmsgbuf);
+	msg.msg_flags = MSG_WAITALL;
+
+	if ( sys_recvmsg(gate, &msg, 0) < 0 ) {
+		return -1;
+	}
+
+	struct cmsghdr *cmsg = (struct cmsghdr*)cmsgbuf;
+	return *((int *)CMSG_DATA(cmsg));
+}
+*/
+
+
+int sendfd(int gate, int fd, void* message, int message_len)
+{
+	int ret;
+	struct iovec iov;
+	char cmsgbuf[CMSG_SPACE(sizeof(int))];
+//	char cmsgbuf[(_ALIGN(sizeof(struct cmsghdr)) + _ALIGN(sizeof(int)))];
+
+	iov.iov_base = message;
+	iov.iov_len = message_len;
+
+	struct linux_cmsghdr *cmsg = (struct linux_cmsghdr*)cmsgbuf;
+//	struct cmsghdr *cmsg = (struct cmsghdr*)cmsgbuf;
+	cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+//	cmsg->cmsg_len = (_ALIGN(sizeof(struct cmsghdr)) + sizeof(int));
+	cmsg->cmsg_level = SOL_SOCKET;
+	cmsg->cmsg_type = SCM_RIGHTS;
+	*((int *)CMSG_DATA(cmsg)) = fd;
+//	*((int *)((void*)((char*)cmsg + _ALIGN(sizeof(struct cmsghdr))))) = fd;
+
+	//struct msghdr msg;
+	struct linux_msghdr msg;
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = cmsgbuf;
+	msg.msg_controllen = sizeof(cmsgbuf);
+	msg.msg_flags = 0;
+
+	if ((ret = sys_sendmsg(gate, &msg, 0)) < 0) {
+		std_printf("err sendmsg %d\n", ret);
+		return -1;
+	}
+	std_printf("fini sendmsg\n");
+	return 0;
+}
+
+
+
+int drain_fds(void)
+{
+	int msg = sys_getpid();
+	int gate = connect_gate("/local.sock2");
+	sendfd(gate, 3, &msg, sizeof(msg));
+	return 0;
+}
+
+
 static int hp(struct hello_pid *hellop){
 	char ch[] = "Hi, LOCAL. I'm DAEMON";
 	memcpy(hellop->hello, ch, sizeof(ch));
@@ -95,6 +205,7 @@ int connection(void *data){
 		sys_write(1, st, 15);
 	}
 
+	std_printf("path %s, family %d\n", args->h_addr.sun_path, args->h_addr.sun_family);
 	if(sys_connect(tsock, (struct sockaddr *)&args->h_addr, args->h_addr_len) < 0){
 	}
 
@@ -123,7 +234,7 @@ int connection(void *data){
 				break;
 			case PARASITE_CMD_DRAIN_FDS:
 				std_printf("drain\n");
-				//drain_fds(data);
+				drain_fds();
 				break;
 		}
 		__parasite_daemon_reply_ack(m.cmd, ret); 
