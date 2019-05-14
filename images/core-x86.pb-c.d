5b25c7215219efe69f8daf213b3f223ced278e03
YuheiTakagawa <b1014223@fun.ca.jp>
2019-05-15 05:26:33 +0900

    feat: add .proto

---
+++ images/core-x86.pb-c.d
@@ -0,0 +1,12 @@
+images/core-x86.pb-c.d images/core-x86.pb-c.o: images/core-x86.pb-c.c \
+ images/core-x86.pb-c.h /usr/local/include/protobuf-c/protobuf-c.h \
+ /usr/include/assert.h /usr/include/sys/cdefs.h /usr/include/limits.h \
+ /usr/include/sys/limits.h /usr/include/machine/_limits.h \
+ /usr/include/x86/_limits.h /usr/include/sys/syslimits.h \
+ /usr/local/lib/gcc6/gcc/x86_64-portbld-freebsd11.1/6.4.0/include/stddef.h \
+ /usr/include/sys/_types.h /usr/include/machine/_types.h \
+ /usr/include/x86/_types.h \
+ /usr/local/lib/gcc6/gcc/x86_64-portbld-freebsd11.1/6.4.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/machine/_stdint.h \
+ /usr/include/x86/_stdint.h /usr/include/sys/_stdint.h images/opts.pb-c.h \
+ images/google/protobuf/descriptor.pb-c.h
---
+++ images/ipc-shm.pb-c.d
@@ -0,0 +1,12 @@
+images/ipc-shm.pb-c.d images/ipc-shm.pb-c.o: images/ipc-shm.pb-c.c \
+ images/ipc-shm.pb-c.h /usr/local/include/protobuf-c/protobuf-c.h \
+ /usr/include/assert.h /usr/include/sys/cdefs.h /usr/include/limits.h \
+ /usr/include/sys/limits.h /usr/include/machine/_limits.h \
+ /usr/include/x86/_limits.h /usr/include/sys/syslimits.h \
+ /usr/local/lib/gcc6/gcc/x86_64-portbld-freebsd11.1/6.4.0/include/stddef.h \
+ /usr/include/sys/_types.h /usr/include/machine/_types.h \
+ /usr/include/x86/_types.h \
+ /usr/local/lib/gcc6/gcc/x86_64-portbld-freebsd11.1/6.4.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/machine/_stdint.h \
+ /usr/include/x86/_stdint.h /usr/include/sys/_stdint.h \
+ images/ipc-desc.pb-c.h
